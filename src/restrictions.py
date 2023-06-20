106967bf3743845f3e3413824b9078cff0a3081c
Eduardo Gutierrez <eduardogutierrez.agc@gmail.com>
2023-06-20 02:45:05 -0400

    center_of_gravity

--- src/restrictions.py
+++ src/restrictions.py
@@ -1,4 +1,6 @@
 from .base import Block
+import math
+
 
 def center_of_gravity(container: Block):
     total_mass = 0
@@ -10,17 +12,24 @@ def center_of_gravity(container: Block):
         aabb_mass = aabb.volume
         total_mass += aabb_mass
 
-        # Calcula el centro del Aabb en cada eje
+        # Calculate the center of the Aabb in each axis
         center_x += (aabb.xmin + aabb.xmax) / 2 * aabb_mass
         center_y += (aabb.ymin + aabb.ymax) / 2 * aabb_mass
         center_z += (aabb.zmin + aabb.zmax) / 2 * aabb_mass
 
-    # Calcula el centro de gravedad dividiendo por la masa total
+    # Calculate the center of gravity by dividing by the total mass
     center_x /= total_mass
     center_y /= total_mass
     center_z /= total_mass
 
-    return (center_x, center_y, center_z)
+    # Calculate the distance difference
+    container_center_x = container.l / 2
+    container_center_y = container.w / 2
+    container_center_z = container.h / 2
+
+    distance = math.sqrt((center_x - container_center_x)**2 + (center_y - container_center_y)**2 + (center_z - container_center_z)**2)
+
+    return distance
 
 def max_weight_suppoted(container: Block):
     pass
