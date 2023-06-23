18b209aa8b143b4356bc82b14f925e8a53ac5157
Nearlz <patriciobarra.99@gmail.com>
2023-06-22 21:15:58 -0400

    dynamic stability started

--- src/base.py
+++ src/base.py
@@ -61,7 +61,7 @@ class Aabb:
         self.l = xmax-xmin; self.w = ymax-ymin; self.h = zmax-zmin
         self.manhattan = self.xmin + self.ymin + self.zmin
         self.covered_surface = 0
-        self.covered_surface_face = {'X1':0,'X2':0,'Y1':0,'Y2':0,'Z1':0,'Z2':0}
+        self.covered_surface_face = {'X1':0,'X2':0,'Y1':0,'Y2':0}
     
     # returns true if aabb is inside self
     def strict_intersects(self, aabb):
@@ -135,7 +135,7 @@ class Aabb:
         self.volume = (xmax-xmin)*(ymax-ymin)*(zmax-zmin)
         self.l = xmax-xmin; self.w = ymax-ymin; self.h = zmax-zmin
         self.covered_surface = 0
-        self.covered_surface_face = {'X1':0,'X2':0,'Y1':0,'Y2':0,'Z1':0,'Z2':0}
+        self.covered_surface_face = {'X1':0,'X2':0,'Y1':0,'Y2':0}
     
     # returns true if aabb is inside self
     def strict_intersects(self, aabb):
--- src/vcs_function.py
+++ src/vcs_function.py
@@ -10,63 +10,60 @@ def CS_function(blocks,space,p,stored_blocks):
         block = Aabb(x,x+possible_block.l,y,y+possible_block.w,z,z+possible_block.h)
 
         for last in stored_blocks:
+            x_diff_max = max([block.xmin,last.xmin])
+            x_diff_min = min([block.xmax,last.xmax])
+            y_diff_max = max([block.ymin,last.ymin])
+            y_diff_min = min([block.ymax,last.ymax])
+            z_diff_max = max([block.zmin,last.zmin])
+            z_diff_min = min([block.zmax,last.zmax])
             
             if abs(block.xmax - last.xmin) <= (block.xmax - block.xmin) * p and ((block.zmin < last.zmax and block.zmax > last.zmin ) and (block.ymin < last.ymax and block.ymax > last.ymin )):
-                z_diff_max = max([block.zmin,last.zmin])
-                z_diff_min = min([block.zmax,last.zmax])
-                y_diff_max = max([block.ymin,last.ymin])
-                y_diff_min = min([block.ymax,last.ymax])
-                block_value[i]+= (z_diff_min-z_diff_max) * (y_diff_min-y_diff_max)
+                block_value[i]+= ((z_diff_min-z_diff_max) * (y_diff_min-y_diff_max))
 
             elif abs(block.xmin - last.xmax) <= (block.xmax - block.xmin) * p and ((block.zmin < last.zmax and block.zmax > last.zmin ) and (block.ymin < last.ymax and block.ymax > last.ymin )):
-                z_diff_max = max([block.zmin,last.zmin])
-                z_diff_min = min([block.zmax,last.zmax])
-                y_diff_max = max([block.ymin,last.ymin])
-                y_diff_min = min([block.ymax,last.ymax])
-                block_value[i]+= (z_diff_min-z_diff_max) * (y_diff_min-y_diff_max)
+                block_value[i]+= ((z_diff_min-z_diff_max) * (y_diff_min-y_diff_max))
 
             elif abs(block.ymax - last.ymin) <= (block.ymax - block.ymin) * p and ((block.zmin < last.zmax and block.zmax > last.zmin ) and (block.xmin < last.xmax and block.xmax > last.xmin )):
-                z_diff_max = max([block.zmin,last.zmin])
-                z_diff_min = min([block.zmax,last.zmax])
-                x_diff_max = max([block.xmin,last.xmin])
-                x_diff_min = min([block.xmax,last.xmax])
-                block_value[i]+= (z_diff_min-z_diff_max) * (x_diff_min-x_diff_max)
+                block_value[i]+= ((z_diff_min-z_diff_max) * (x_diff_min-x_diff_max))
 
             elif abs(block.ymin - last.ymax) <= (block.ymax - block.ymin) * p and ((block.zmin < last.zmax and block.zmax > last.zmin ) and (block.xmin < last.xmax and block.xmax > last.xmin )):
-                z_diff_max = max([block.zmin,last.zmin])
-                z_diff_min = min([block.zmax,last.zmax])
-                x_diff_max = max([block.xmin,last.xmin])
-                x_diff_min = min([block.xmax,last.xmax])
-                block_value[i]+= (z_diff_min-z_diff_max) * (x_diff_min-x_diff_max)
+                block_value[i]+= ((z_diff_min-z_diff_max) * (x_diff_min-x_diff_max))
 
             elif abs(block.zmax - last.zmin) <= (block.zmax - block.zmin) * p and ((block.ymin < last.ymax and block.ymax > last.ymin ) and (block.xmin < last.xmax and block.xmax > last.xmin )):
-                y_diff_max = max([block.ymin,last.ymin])
-                y_diff_min = min([block.ymax,last.ymax])
-                x_diff_max = max([block.xmin,last.xmin])
-                x_diff_min = min([block.xmax,last.xmax])
-                block_value[i]+= (y_diff_min-y_diff_max) * (x_diff_min-x_diff_max)
+                block_value[i]+= ((y_diff_min-y_diff_max) * (x_diff_min-x_diff_max))
 
             elif abs(block.zmin - last.zmax) <= (block.zmax - block.zmin) * p and ((block.ymin < last.ymax and block.ymax > last.ymin ) and (block.xmin < last.xmax and block.xmax > last.xmin )):
-                y_diff_max = max([block.ymin,last.ymin])
-                y_diff_min = min([block.ymax,last.ymax])
-                x_diff_max = max([block.xmin,last.xmin])
-                x_diff_min = min([block.xmax,last.xmax])
-                block_value[i]+= (y_diff_min-y_diff_max) * (x_diff_min-x_diff_max)
+                block_value[i]+= ((y_diff_min-y_diff_max) * (x_diff_min-x_diff_max))
 
             elif block.xmin <= (block.xmax - block.xmin) * p or abs(block.xmax - space.l) <= (block.xmax - block.xmin) * p:
-                block_value[i]+= (block.ymax-block.ymin) * (block.zmax-block.zmin)
+                block_value[i]+= ((block.ymax-block.ymin) * (block.zmax-block.zmin))
             elif block.ymin <= (block.ymax - block.ymin) * p or abs(block.ymax - space.w) <= (block.ymax - block.ymin) * p:
-                block_value[i]+= (block.xmax-block.xmin) * (block.zmax-block.zmin)
+                block_value[i]+= ((block.xmax-block.xmin) * (block.zmax-block.zmin))
             elif block.zmin <= (block.zmax - block.zmin) * p or abs(block.zmax - space.h) <= (block.zmax - block.zmin) * p:
-                block_value[i]+= (block.ymax-block.ymin) * (block.xmax-block.xmin)
+                block_value[i]+= ((block.ymax-block.ymin) * (block.xmax-block.xmin))
 
     max_index = block_value.index(max(block_value))
     return blocks[max_index]
+
+# def dynamic_stability(blocks,space,p,stored_blocks):
+#     block_value = [0] * len(blocks)
+#     for i,possible_block in enumerate(blocks,0):
+#         x,y,z = space.corner_point
+#         if x == space.xmax: x -= possible_block.l
+#         if y == space.ymax: y -= possible_block.w
+#         if z == space.zmax: z -= possible_block.h
+#         block = Aabb(x,x+possible_block.l,y,y+possible_block.w,z,z+possible_block.h)
+
+#         i_stored_blocks = stored_blocks.append(block)
+#         print(f'{len(i_stored_blocks)}  {len(stored_blocks)}')
+
+
 
 def eval_function(blocks,space,stored_blocks, p:float, test:bool = False) :
     if test == True:
         CS_function(blocks,space,p,stored_blocks)
     else:
         p_blocks = blocks.possible_blocks(space.l, space.w, space.h)
+        # dynamic_stability(p_blocks,space,p,stored_blocks)
         return CS_function(p_blocks,space,p,stored_blocks)
         # return blocks.largest(space.l, space.w, space.h)
